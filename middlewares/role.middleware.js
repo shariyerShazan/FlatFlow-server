e44ef8f3ab1a2f5f2170384bc394b33a4032153c
shariyerShazan <shariyershazan1@gmail.com>
2025-07-11 16:02:31 +0600

    role base middleware

---
+++ middlewares/role.middleware.js
@@ -0,0 +1,31 @@
+import { User } from "../models/user.model.js";
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (user?.role !== "admin") {
+      return res.status(403).json({
+        message: "Forbidden: Only admin allowed",
+        success: false,
+      });
+    }
+    next();
+  } catch (err) {
+    res.status(500).json({ message: "Server error", success: false });
+  }
+};
+
+export const isMember = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (user?.role !== "member") {
+      return res.status(403).json({
+        message: "Forbidden: Only member allowed",
+        success: false,
+      });
+    }
+    next();
+  } catch (err) {
+    res.status(500).json({ message: "Server error", success: false });
+  }
+};
