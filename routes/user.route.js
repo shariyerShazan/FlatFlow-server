3681cf3229a8f7c14775d1e1111e4bbd0c279bae
shariyerShazan <shariyershazan1@gmail.com>
2025-07-24 16:12:10 +0600

    firebase controller for user login

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllmember, getDatas, login, logout, register, removeMember } from "../controllers/user.controller.js";
+import { firebaseLogin, getAllmember, getDatas, login, logout, register, removeMember } from "../controllers/user.controller.js";
 import { isAuthed } from "../middlewares/isAuthed.js";
 import upload from "../middlewares/multer.js";
 import { isAdmin } from "../middlewares/role.middleware.js";
@@ -10,7 +10,7 @@ const router = express.Router();
 // Public
 router.post("/register", upload.single("profilePicture") , register);
 router.post("/login", login);
-
+router.post("/firebase-login", firebaseLogin);
 //  Authenticated User
 router.post("/logout", isAuthed, logout);
 
@@ -20,5 +20,7 @@ router.get("/datas", isAuthed, isAdmin, getDatas);
 
 router.get("/get-all-members", isAuthed, isAdmin, getAllmember);
 router.post("/remove-member/:memberId", isAuthed, isAdmin, removeMember);
+
+
 
 export default router;
